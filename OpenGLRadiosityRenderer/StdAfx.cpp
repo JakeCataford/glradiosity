b38437f1bb1a06cdcc747c0f5dfd9eb0f93876e4
JakeCataford <JakeCataford@Gmail.com>
2014-04-20 23:20:28 -0400

    Initial Commit

---
+++ OpenGLRadiosityRenderer/StdAfx.cpp
@@ -0,0 +1,14 @@
+//
+//  StdAfx.cpp
+//  OpenGLRadiosityRenderer
+//
+//  Created by Jake Cataford on 4/20/14.
+//    Copyright (c) 2014 Carleton University. All rights reserved.
+//
+
+//-----------------------------------------------------------------------------
+//------ StdAfx.cpp : source file that includes just the standard includes
+//------  StdAfx.pch will be the pre-compiled header
+//------  StdAfx.obj will contain the pre-compiled type information
+//-----------------------------------------------------------------------------
+#include "StdAfx.h"
